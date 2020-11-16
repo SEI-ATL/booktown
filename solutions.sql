44a3e609a2681084e21ed613aa6e4763aeefdb34
Lev Michael <lev.choubine@gmail.com>
2020-11-16 13:08:04 -0500

    solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,16 +1,22 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+SELECT subject_id FROM books ORDER BY subject_id
 -- 2. Find all subjects sorted by location
-
+SELECT subject FROM subjects ORDER BY location;
+     subject 
 -- ### Where
 -- 3. Find the book "Little Women"
+SELECT * FROM books WHERE title ='Little Women';
 -- 4. Find all books containing the word "Python"
+SELECT * FROM books WHERE title  LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+SELECT * FROM subjects WHERE location ='Main St' ORDER BY subject;
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+SELECT * FROM books WHERE subject_id =(SELECT id FROM subjects WHERE subject='Computers');
+-- 7. Find all books and display a result table with ONLY the following columns
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
