ab45954aab5b1ad933d81d402039bf656314e926
alexjbustillos <alexjbustillos@gmail.com>
2020-11-16 13:00:42 -0500

    [JOIN]Question 8

--- solutions.sql
+++ solutions.sql
@@ -1,24 +1,48 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+SELECT * 
+FROM subjects
+ORDER BY subject;
 -- 2. Find all subjects sorted by location
-
+SELECT * 
+FROM subjects 
+ORDER BY location;
 -- ### Where
 -- 3. Find the book "Little Women"
+SELECT * 
+FROM books 
+WHERE title = 'Little Women';
 -- 4. Find all books containing the word "Python"
+SELECT * 
+FROM books 
+WHERE title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+SELECT *
+FROM subjects
+WHERE location = 'Main St'
+ORDER BY subject;
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+SELECT title
+FROM books
+JOIN subjects
+ON books.subject_id = subjects.id
+WHERE subject = 'Computers';
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+SELECT books.title, authors.first_name, authors.last_name, subjects.subject
+FROM books
+JOIN authors ON authors.id = books.author_id
+JOIN subjects ON subjects.id = books.subject_id;
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
