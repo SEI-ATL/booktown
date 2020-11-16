326c94a8bc3b929317efaf2c959f8325e7871a16
Will Locklier <will.locklier@gmail.com>
2020-11-16 13:00:23 -0500

    [solutions] Add solutions

--- solutions.sql
+++ solutions.sql
@@ -1,17 +1,24 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+SELECT * FROM subjects ORDER BY subject;
 -- 2. Find all subjects sorted by location
-
+SELECT * FROM subjects ORDER BY location;
 -- ### Where
 -- 3. Find the book "Little Women"
+SELECT * FROM books WHERE title = 'Little Women';
 -- 4. Find all books containing the word "Python"
+SELECT * FROM books WHERE title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+SELECT books.title
+FROM book_queue
+INNER JOIN books ON book_queue.subject_id=books.subject_id;
 -- 7. Find all books and display a result table with ONLY the following columns
+SELECT books.title, authors.first_name, authors.last_name,
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
@@ -33,6 +40,13 @@
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+SELECT COUNT(*)
+FROM books;
 -- 12. Get the COUNT of all Locations
+SELECT COUNT(location)
+FROM subjects;
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+SELECT location, COUNT(location)
+FROM subjects
+GROUP BY location;
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
